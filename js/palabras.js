8a19e2af8d5a96b08392bd496b70fc6094e1b548
Leonardo <leonardonarvaja232@gmail.com>
2022-02-12 23:45:34 -0300

    Ajustes en los mensajes de error

--- js/palabras.js
+++ js/palabras.js
@@ -2,9 +2,11 @@ var lista = ["FOSIL", "DEBIL", "HOMICIDIO", " PROBAR", "EVITAR", "NIÑOS", "HIBE
 
 var botonAgregar = document.querySelector("#nueva-palabra");
 
-var error = document.querySelector("#span-error")
+var error = document.querySelector("#span-error");
 
 botonAgregar.addEventListener("click", function(event) {
+
+    console.log(lista);
 
     let input = document.querySelector("#input-nueva-palabra");
 
@@ -12,13 +14,20 @@ botonAgregar.addEventListener("click", function(event) {
 
     setTimeout(function(){error.innerHTML = "";},3000);
 
-    recibirPalabra(input);
+    if(lista.includes(input.value)) {
+        error.innerHTML = "*Ya existe la palabra";
+        error.classList.add("mensaje-error");
+    } else {
+        if(verificarPalabra(input)) {
+            recibirPalabra(input);
+
+        }        
+    }
 
     
 })
 
 function recibirPalabra(input) {
-
 
     let palabraNueva = input.value;
 
@@ -26,21 +35,39 @@ function recibirPalabra(input) {
    
     const comparacionMin = new RegExp(/[a-z]/);
 
-    if(verificacion.test(palabraNueva)) {
-        error.innerHTML = "*Debe ingresar una palabra solo con letras";
-        error.classList.add("mensaje-error");
-    } else {
-        if(comparacionMin.test(palabraNueva)) {
-            error.innerHTML = "*Solo letras mayúsculas";
+        if(verificacion.test(palabraNueva)) {
+            error.innerHTML = "*Debe ingresar una palabra solo con letras";
             error.classList.add("mensaje-error");
         } else {
-            lista.push(palabraNueva);
-            error.innerHTML = "*Palabra adicionada, Muchas gracias!!";
-            error.classList.add("mensaje-correcto");
-            input.value = "";
+            if(comparacionMin.test(palabraNueva)) {
+                error.innerHTML = "*Solo letras mayúsculas";
+                error.classList.add("mensaje-error");
+            } else {
+                lista.push(palabraNueva);
+                error.innerHTML = "*Palabra adicionada, Muchas gracias!!";
+                error.classList.add("mensaje-correcto");
+                input.value = "";
+            }
         }
-
     }
 
+function verificarPalabra(input,lista) {
+
+    let resultado;
+
+    if(input.value.length > 14 || input.value.length < 2 && input.value.length != 0) {
+        error.innerHTML = "*La palabra es muy larga o muy corta";
+        error.classList.add("mensaje-error");
+        resultado = false;
+    } else {
+        if(input.value == "") {
+            error.innerHTML = "*Debes ingresar una palabra";
+            error.classList.add("mensaje-error");
+            resultado = false;
+        } else {
+            resultado = true;
+        }
+    }
+    return resultado;
 }
 
